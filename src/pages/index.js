a9898d09def434bfaff7f796192795bf820cd607
Chris Ingram <chris@chrisingram.me>
2017-08-06 11:55:53 +0100

    initial gatsby setup

---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import Radium from 'radium';
+import Color from 'color';
+
+import Reveal from 'react-reveal';
+import 'animate.css/animate.css';
+
+class IndexPage extends React.Component{
+
+  // render(){
+  //   return (
+  //   <div>
+  //     <section>
+  //       <h1 style={styles}>Hi people</h1>
+  //       <p>Welcome to your new Gatsby site.</p>
+  //       <p>Now go build something great.</p>
+  //       <Link to="/page-2/">Go to page 2</Link>
+  //     </section>
+  //     <Reveal effect="animated fadeIn" style={{'-vendor-animation-duration': '0.2s'}}>
+  //       <section>
+  //         <h1 style={{color: 'green'}}>Reveal Section</h1>
+  //         <p>Welcome to your new Gatsby site.</p>
+  //         <p>Now go build something great.</p>
+  //         <Link to="/page-2/">Go to page 2</Link>
+  //       </section>
+  //     </Reveal>
+  //   </div>
+  //   );
+  // }
+
+  render(){
+    return (<div></div>)
+  }
+
+}
+
+var styles = {
+  color: '#ff0000'
+}
+
+export default IndexPage

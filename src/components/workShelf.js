ec7f1adae67a16ae54d30b3d5f20afc223126d05
Chris Ingram <chris@chrisingram.me>
2017-08-15 00:07:38 +0100

    work shelf: finished styling

--- src/components/workShelf.js
+++ src/components/workShelf.js
@@ -9,11 +9,16 @@ const WorkShelf = () =>
   <section className="diagonal clockwise" style={styles.section}>
     <h2 style={sharedStyles.sectionHeading}>Some Of My Work</h2>
 
-    <div className="containerFluid" style={styles.container}>
-      <div className="row">
-        <div className="col-sm-6"><img src={reebok1} style={styles.featuredImage} /></div>
+    <div className="container-fluid" style={styles.container}>
+      <div className="row no-gutters">
+        <div className="col-sm-6">
+          <div styles={styles.shadowImage}>
+            <span style={styles.imageBefore}></span>
+            <img src={reebok1} style={styles.featuredImage} />
+          </div>
+        </div>
         <div className="col-sm-6" style={styles.rightHalf}>
-          <h4 style={styles.h4}>FEATURED: Reebok Showcase</h4>
+          <h4 style={styles.h4}><span style={styles.h4Before}></span>FEATURED: Reebok Showcase</h4>
           <p style={styles.p}>A presentation designed for a presentation on sexism in sport. The concept evolved from the idea of women ‘breaking’ and ‘tearing’ away from previous stereotypes that have causes their subordination in sports, even in the 21st century.</p>
           <p style={styles.p}>Click the link for more images of the project.</p>
         </div>
@@ -28,26 +33,56 @@ styles.section = {
 }
 
 styles.container = {
-  marginTop: "7%"
+  marginTop: "5%",
+  padding: 0
 }
 
 styles.featureImage = {
-  width: "100%"
+  width: "100%",
+  position: "relative",
+  zIndex: 1
+}
+
+styles.shadowImage = {
+  position: "relative"
+}
+
+styles.imageBefore = {
+  position: "absolute",
+  width: "102%",
+  height: "92%",
+  top: "-6%",
+  left: 0,
+  background: "#a8e4bc",
+  zIndex: -1
 }
 
 styles.rightHalf = {
-  padding: "3%"
+  padding: "3% 6%"
 }
 
 styles.h4 = {
   fontFamily: "RobotoMono-Regular",
-  marginBottom: "9%"
+  marginBottom: "9%",
+  position: "relative",
+  display: "inline-block"
+}
+
+styles.h4Before = {
+  position: "absolute",
+  width: "110%",
+  height: "92%",
+  top: "10px",
+  left: 0,
+  background: "#fdc523",
+  zIndex: -1
 }
 
 styles.p = {
   fontFamily: "Apercu-Regular",
   fontSize: "20px",
-  color: "#b2b1b8"
+  color: "#b2b1b8",
+  lineHeight: "1.7"
 }
 
 export default WorkShelf;

0afca9d3d9d97bf72039d6d6dbc97bef9cd4cd95
Chris Ingram <chris@chrisingram.me>
2017-08-14 23:42:30 +0100

    work shelf: added fonts and some styling

--- src/components/workShelf.js
+++ src/components/workShelf.js
@@ -9,11 +9,11 @@ const WorkShelf = () =>
   <section className="diagonal clockwise" style={styles.section}>
     <h2 style={sharedStyles.sectionHeading}>Some Of My Work</h2>
 
-    <div className="containerFluid">
+    <div className="containerFluid" style={styles.container}>
       <div className="row">
         <div className="col-sm-6"><img src={reebok1} style={styles.featuredImage} /></div>
         <div className="col-sm-6" style={styles.rightHalf}>
-          <h4 style={styles.h4}>Featured: Reebok Showcase</h4>
+          <h4 style={styles.h4}>FEATURED: Reebok Showcase</h4>
           <p style={styles.p}>A presentation designed for a presentation on sexism in sport. The concept evolved from the idea of women ‘breaking’ and ‘tearing’ away from previous stereotypes that have causes their subordination in sports, even in the 21st century.</p>
           <p style={styles.p}>Click the link for more images of the project.</p>
         </div>
@@ -27,6 +27,10 @@ styles.section = {
   background: "white"
 }
 
+styles.container = {
+  marginTop: "7%"
+}
+
 styles.featureImage = {
   width: "100%"
 }
@@ -36,11 +40,14 @@ styles.rightHalf = {
 }
 
 styles.h4 = {
-
+  fontFamily: "RobotoMono-Regular",
+  marginBottom: "9%"
 }
 
 styles.p = {
-  fontFamily: "Apercu-Regular"
+  fontFamily: "Apercu-Regular",
+  fontSize: "20px",
+  color: "#b2b1b8"
 }
 
 export default WorkShelf;

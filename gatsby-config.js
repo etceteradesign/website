a168643365c98e2d81a13f33e83e105f49831bdd
Chris Ingram <chris@chrisingram.me>
2017-09-27 18:45:21 +0100

    added nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    { resolve: `gatsby-plugin-nprogress`,
+      options: {
+      // Setting a color is optional.
+      color: `tomato`,
+      // Disable the loading spinner.
+      showSpinner: true,
+    }
+  }
+  ]
 }

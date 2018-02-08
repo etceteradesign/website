cb799af6e82340d5ff4d91798b8bdb4cae60b5d2
Chris Ingram <chris@chrisingram.me>
2018-02-08 22:22:36 +0000

    made images list in projects model

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link';
 
 import ProjectPage from '../components/projects/projectPage';
 
-import bg from '../assets/projects/buffalo7/buffalo7-header-bg.jpg';
-
-import infographic from '../assets/projects/buffalo7/buffalo7-1.jpg';
+// import bg from '../assets/projects/buffalo7/buffalo7-header-bg.jpg';
+//
+// import infographic from '../assets/projects/buffalo7/buffalo7-1.jpg';
 
 class ProjectTemplate extends React.Component{
 
@@ -14,13 +14,14 @@ class ProjectTemplate extends React.Component{
     const { markdownRemark } = this.props.data;
     const { frontmatter, html } = markdownRemark;
 
+    let images = [frontmatter.images]
 
     return (
       <ProjectPage
-        bg={bg}
+        bg={frontmatter.headerBG}
         heading={frontmatter.title}
         description={frontmatter.description}
-        images={[infographic]}
+        images={images}
         prevPageName={frontmatter.prevPageName}
         nextPageName={frontmatter.nextPageName} />
     )
@@ -37,7 +38,10 @@ export const pageQuery = graphql`
       frontmatter {
         path
         title
+        date
         description
+        headerBG
+        images
         prevPageName
         nextPageName
       }

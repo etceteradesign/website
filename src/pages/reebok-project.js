f8384f7cee695c4eb34331883ffc84eeae2a7870
Chris Ingram <chris@chrisingram.me>
2018-02-10 15:11:41 +0000

    added reebok project

---
+++ src/pages/reebok-project.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import ProjectPage from '../components/projects/projectPage';
+
+import bg from '../assets/projects/reebok/reebok-header-bg.jpg';
+
+import reebok1 from '../assets/projects/reebok/reebok1.jpg';
+import reebok2 from '../assets/projects/reebok/reebok2.jpg';
+import reebok3 from '../assets/projects/reebok/reebok3.jpg';
+import reebok4 from '../assets/projects/reebok/reebok4.jpg';
+
+class ReebokProject extends React.Component{
+
+  render(){
+    return (<div>
+      <ProjectPage
+        bg={bg}
+        heading="Reebok Showcase"
+        description="A presentation designed for an imaginary brief for Reebok on sexism in sport. The concept evolved from the idea of women ‘breaking’ and ‘tearing’ away from previous stereotypes that have caused their subordination in sports, even in the 21st century."
+        images={[reebok1, reebok2, reebok3, reebok4]}
+        prevPageName='#'
+        nextPageName='/buffalo7-infographic' />
+    </div>)
+  }
+
+}
+
+
+
+export default ReebokProject;
